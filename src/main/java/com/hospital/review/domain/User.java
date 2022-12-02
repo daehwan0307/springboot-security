64b5abd643f909fab5f8380e4217eeae859a5672
daehwan0307 <eoghks960307@naver.com>
2022-12-02 13:28:08 +0900

    test

--- src/main/java/com/hospital/review/domain/User.java
+++ src/main/java/com/hospital/review/domain/User.java
@@ -22,6 +22,6 @@ public class User {
     @Column(unique = true)
     private String userName;
     private String password;
-    private String emailAddress;
+    private String emailAddress;//
 
 }
