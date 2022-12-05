20df7a6a63a7303b12ed71b8e222416a4d85d966
daehwan0307 <eoghks960307@naver.com>
2022-12-05 13:27:04 +0900

    test code 작성

--- src/test/java/com/hospital/review/controller/UserControllerTest.java
+++ src/test/java/com/hospital/review/controller/UserControllerTest.java
@@ -1,10 +1,9 @@
 package com.hospital.review.controller;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hospital.review.domain.dto.UserDto;
 import com.hospital.review.domain.dto.UserJoinRequest;
-import com.hospital.review.exception.ErrorCode;
-import com.hospital.review.exception.HospitalReviewAppException;
 import com.hospital.review.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,14 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,66 +30,28 @@ class UserControllerTest {
     ObjectMapper objectMapper;
 
     @Test
-    @DisplayName("회원가입 성공")
-    @WithMockUser
-    void join_success() throws Exception {
-        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
-                .userName("kyeongrok1")
-                .password("1q2w3e4r")
-                .email("oceanfog1@gmail.com")
-                .build();
-
-        when(userService.join(any())).thenReturn(mock(UserDto.class));
-
+    @DisplayName("회원가입성공")
+    void  join() throws Exception {
+        String userName = "eoghks";
+        String password="1234";
+        String email="eoghks@naver.com";
         mockMvc.perform(post("/api/v1/users/join")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName,password,email))))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("회원가입 실패")
-    void join_fail() throws Exception {
-
-        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
-                .userName("kyeongrok")
-                .password("1q2w3e4r")
-                .email("oceanfog1@gmail.com")
-                .build();
-
-        when(userService.join(any())).thenThrow(new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
-
+    @DisplayName("회원가입 실패 - userName 중복")
+    void  join_fail() throws Exception {
+        String userName = "eoghks";
+        String password="1234";
+        String email="eoghks@naver.com";
         mockMvc.perform(post("/api/v1/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName,password,email))))
                 .andDo(print())
                 .andExpect(status().isConflict());
     }
-    @Test
-    @DisplayName("로그인 실패 - id없음")
-    @WithMockUser
-    void login_fail1() throws Exception {
-//        String id = "kyeongrok";
-//        String password = "1q2w3e4r";
-        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
-                .userName("kyeongrok")
-                .password("1q2w3e4r")
-                .email("oceanfog1@gmail.com")
-                .build();
-
-        // id, pw를 보내서
-        when(userService.login(any(), any())).thenThrow(new HospitalReviewAppException(ErrorCode.NOT_FOUND, ""));
-
-        // NOT_FOUND를 받으면 잘 만든 것이다
-        mockMvc.perform(post("/api/v1/users/join")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
 }
-
